507f3e6bcbcbc5f5afdcaf1b873903350b8d442b
Joao <joaosilva2505@outlook.comgit config --global user.name joaosilva25>
2023-09-29 15:36:44 -0300

    latest 1.0

--- script.js
+++ script.js
@@ -1,20 +1,204 @@
 let listPlayers =[]
 let listClasses =[]
-
-let button=document.querySelector(".add_player")
-
-
+let table = document.querySelector('table')
+let example = document.querySelector('.example')
+let td =document.querySelectorAll('td')
+let tr=document.querySelectorAll('tr')
 
 function addPlayer() {
     let selectPlayers=document.querySelector(".nomes")
     let selectClass=document.querySelector(".classe")
 
-    listPlayers.push(selectPlayers.value)
-    listClasses.push(selectClass.value)
 
+    if(example) {
+        example.remove()
+    }
+
+    if  (!listPlayers.includes(selectPlayers.value)) {
+        listPlayers.push(selectPlayers.value)
+        listClasses.push(selectClass.value) 
+    }
 
     console.log(listPlayers)
     console.log(listClasses)
 
 
+    table.innerHTML = '';
+
+    let topTr=document.createElement('tr')
+    let nomeTh=document.createElement('th')
+    let classeTh=document.createElement('th')
+    table.appendChild(topTr)
+    topTr.appendChild(nomeTh)
+    topTr.appendChild(classeTh)
+
+
+    nomeTh.innerText = 'Nome'
+    classeTh.innerText='Classe'
+
+
+    for(let i=0;i<listPlayers.length;i++) {
+
+        let newTr=document.createElement('tr')
+        newTr.classList.add('trNew')
+        let newTdLeft=document.createElement('td')
+        newTdLeft.classList.add('tdLeft')
+        let newTdRight=document.createElement('td')
+        newTdRight.classList.add('tdRight')
+
+
+        table.appendChild(newTr)
+        newTr.appendChild(newTdLeft)
+        newTr.appendChild(newTdRight)
+        
+        newTdLeft.innerText = listPlayers[i]
+        newTdRight.innerText = listClasses[i]
+    }
+
+    function saveData() {
+        localStorage.setItem('listPlayers',JSON.stringify(listPlayers))
+        localStorage.setItem('listClasses',JSON.stringify(listClasses))
+    }
+
+    window.addEventListener('unload',saveData)
+
+
+}
+
+
+function generateTeam() {
+    let objectPlayers =[]
+
+
+    for (let i=0; i<listPlayers.length; i++) {
+        let newObject= {
+            nome: `${listPlayers[i]}`,
+            classe: `${listClasses[i]}`
+        };
+        objectPlayers.push(newObject)
+
+  }
+
+    listClassA=[]
+    listClassB=[]
+    listClassC=[]
+
+    for(let r=0;r<objectPlayers.length;r++) {
+        if(objectPlayers[r].classe.includes("A")) {
+            listClassA.push(objectPlayers[r]);
+        }
+        else if(objectPlayers[r].classe.includes("B")) {
+            listClassB.push(objectPlayers[r]);
+        }
+        else if(objectPlayers[r].classe.includes("C")) {
+            listClassC.push(objectPlayers[r])
+        }
+    }
+
+    console.log("list class abaixo:")
+    console.log(listClassA)
+    console.log(listClassB)
+    console.log(listClassC)
+
+
+    let redTeam=[]
+    let greenTeam=[]
+    let blackTeam=[]
+    let whiteTeam=[]
+
+    function getRandomIndex(max) {
+       return Math.floor(Math.random()*max)
+    }
+
+
+    while(redTeam.length<4) {
+        random1=getRandomIndex(listClassA.length)
+        redTeam.push(listClassA.splice(random1, 1)[0]);
+        random2=getRandomIndex(listClassB.length)
+        redTeam.push(listClassB.splice(random2, 1)[0]);
+        random3=getRandomIndex(listClassB.length)
+        redTeam.push(listClassB.splice(random3, 1)[0]);
+        random4=getRandomIndex(listClassC.length)
+        redTeam.push(listClassC.splice(random4, 1)[0]);
+    }
+
+    while(greenTeam.length<4) {
+        random2=getRandomIndex(listClassA.length)
+        greenTeam.push(listClassA.splice(random2, 1)[0]);
+        random3=getRandomIndex(listClassB.length)
+        greenTeam.push(listClassB.splice(random3, 1)[0]);
+        random4=getRandomIndex(listClassB.length)
+        greenTeam.push(listClassB.splice(random4, 1)[0]);
+        random5=getRandomIndex(listClassC.length)
+        greenTeam.push(listClassC.splice(random5, 1)[0]);
+    }
+
+    if(listClassA.length > 0 || listClassB.length > 0 || listClassC.length > 0) {
+        random6=getRandomIndex(listClassA.length)
+        blackTeam.push(listClassA.splice(random6, 1)[0]);
+        random7=getRandomIndex(listClassB.length)
+        blackTeam.push(listClassB.splice(random7, 1)[0]);
+        random8=getRandomIndex(listClassB.length)
+        blackTeam.push(listClassB.splice(random8, 1)[0]);
+        random9=getRandomIndex(listClassC.length)
+        blackTeam.push(listClassC.splice(random9, 1)[0]);
+
+        }
+
+    if(listClassA.length > 0 || listClassB.length > 0 || listClassC.length > 0) {
+        random10=getRandomIndex(listClassA.length)
+        whiteTeam.push(listClassA.splice(random10, 1)[0]);
+        random11=getRandomIndex(listClassB.length)
+        whiteTeam.push(listClassB.splice(random11, 1)[0]);
+        random12=getRandomIndex(listClassB.length)
+        whiteTeam.push(listClassB.splice(random12, 1)[0]);
+        random13=getRandomIndex(listClassC.length)
+        whiteTeam.push(listClassC.splice(random13, 1)[0]);
+        }
+
+
+    console.log("list class abaixo2:")
+    console.log(listClassA)
+    console.log(listClassB)
+    console.log(listClassC)
+    
+    console.log("Time Vermelho")
+    console.log(redTeam)
+    console.log("Time Verde")
+    console.log(greenTeam)
+    console.log("Time Preto")
+    console.log(blackTeam)
+    console.log("Time Branco")
+    console.log(whiteTeam)
+
+    localStorage.setItem('redTeam',JSON.stringify(redTeam))
+    localStorage.setItem('greenTeam',JSON.stringify(greenTeam))
+    localStorage.setItem('blackTeam',JSON.stringify(blackTeam))
+    localStorage.setItem('whiteTeam',JSON.stringify(whiteTeam))
+
+}
+
+
+
+function eraser() {
+
+    let trNew=document.querySelectorAll('.trNew')
+    let tdLeft=document.querySelectorAll('.tdLeft')
+    let tdRight=document.querySelectorAll('.tdRight')
+
+    listPlayers.splice(0)
+    listClasses.splice(0)
+
+    trNew.forEach((tr)=> {
+        tr.remove()
+    })
+    tdLeft.forEach((td)=> {
+        td.remove()
+    })
+    tdRight.forEach((td)=> {
+        td.remove()
+    })
+
+    table.appendChild(example)
+  
 }
--- scriptGR.js
+++ scriptGR.js
@@ -3,8 +3,10 @@ let teamTwoLoading =document.querySelector('.loading_two')
 let teamOne=document.querySelector('.team_one');
 let teamTwo=document.querySelector('.team_two');
 
+  
 
 function firstMatch() {
+
 
     teamOneLoading.remove();
     teamTwoLoading.remove();
@@ -20,12 +22,87 @@ function firstMatch() {
     list.splice(listLeftRandom,1)
 
     console.log(list)
-
+    
     let listRightRandom = Math.floor(Math.random()*list.length)
     let randomRight = list[listRightRandom]
 
     console.log(randomRight);
 
+    setTimeout(()=> {
     teamOne.innerHTML= `<h3>${randomLeft}</h3>`
     teamTwo.innerHTML = `<h3>${randomRight}</h3>`
+    },1500)
+
+
+
 }   
+
+window.onload = function formatedTeams() {
+
+    const redTeam=JSON.parse(localStorage.getItem('redTeam'))
+    const greenTeam=JSON.parse(localStorage.getItem('greenTeam'))
+    const blackTeam=JSON.parse(localStorage.getItem('blackTeam'))
+    const whiteTeam=JSON.parse(localStorage.getItem('whiteTeam'))
+
+    console.log(redTeam)
+    console.log(greenTeam)
+    console.log(blackTeam)
+    console.log(whiteTeam)
+
+    let redOne=document.querySelector('.red_one');
+    let redTwo=document.querySelector('.red_two');
+    let redThree=document.querySelector('.red_three');
+    let redFour=document.querySelector('.red_four');
+
+    let greenOne=document.querySelector('.green_one');
+    let greenTwo=document.querySelector('.green_two');
+    let greenThree=document.querySelector('.green_three');
+    let greenFour=document.querySelector('.green_four');
+
+    let blackOne=document.querySelector('.black_one');
+    let blackTwo=document.querySelector('.black_two');
+    let blackThree=document.querySelector('.black_three');
+    let blackFour=document.querySelector('.black_four');
+
+    let whiteOne=document.querySelector('.white_one');
+    let whiteTwo=document.querySelector('.white_two');
+    let whiteThree=document.querySelector('.white_three');
+    let whiteFour=document.querySelector('.white_four');
+
+
+    redOne.innerText=redTeam[0].nome
+    redTwo.innerText=redTeam[1].nome
+    redThree.innerText=redTeam[2].nome
+    redFour.innerText=redTeam[3].nome
+
+
+    greenOne.innerText=greenTeam[0].nome
+    greenTwo.innerText=greenTeam[1].nome
+    greenThree.innerText=greenTeam[2].nome
+    greenFour.innerText=greenTeam[3].nome
+
+    for (let i = 0; i < blackTeam.length; i++) {
+        if (blackTeam[i] === null || blackTeam[i] === undefined) {
+            blackTeam.splice(i, 1);
+        }
+    }
+
+    
+    blackOne.innerText=blackTeam[0].nome
+    blackTwo.innerText=blackTeam[1].nome
+    blackThree.innerText=blackTeam[2].nome
+    blackFour.innerText=blackTeam[3].nome
+
+
+    for(let r=0; r< whiteTeam.length; r++) {
+        if(whiteTeam[r] === null || whiteTeam[r] === undefined) {
+            whiteTeam.splice(r,1)
+        }
+    }
+
+
+    whiteOne.innerText= whiteTeam[0].nome
+    whiteTwo.innerText= whiteTeam[1].nome
+    whiteThree.innerText= whiteTeam[2].nome
+    whiteFour.innerText= whiteTeam[3].nome
+}
