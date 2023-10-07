ee3009b2af0fafa5e3395a831f93b4bc268cfd91
Joao <joaosilva2505@outlook.comgit config --global user.name joaosilva25>
2023-10-06 21:34:52 -0300

    version1.0.2

--- scriptGR.js
+++ scriptGR.js
@@ -3,51 +3,48 @@ let teamTwoLoading =document.querySelector('.loading_two')
 let teamOne=document.querySelector('.team_one');
 let teamTwo=document.querySelector('.team_two');
 
-  
 
 function firstMatch() {
-
 
     teamOneLoading.remove();
     teamTwoLoading.remove();
 
-    let list=["Vermelho","Preto","Branco","Verde"]
+    let list=["Vermelho","Preto","Branco","Verde"];
 
-    let listLeftRandom = Math.floor(Math.random()*list.length)
+    let listLeftRandom = Math.floor(Math.random()*list.length);
 
-    let randomLeft = list[listLeftRandom]
+    let randomLeft = list[listLeftRandom];
 
     console.log(randomLeft);
 
-    list.splice(listLeftRandom,1)
+    list.splice(listLeftRandom,1);
 
-    console.log(list)
+    console.log(list);
     
-    let listRightRandom = Math.floor(Math.random()*list.length)
-    let randomRight = list[listRightRandom]
+    let listRightRandom = Math.floor(Math.random()*list.length);
+    let randomRight = list[listRightRandom];
 
     console.log(randomRight);
 
     setTimeout(()=> {
-    teamOne.innerHTML= `<h3>${randomLeft}</h3>`
-    teamTwo.innerHTML = `<h3>${randomRight}</h3>`
-    },1500)
-
+    teamOne.innerHTML= `<h3>${randomLeft}</h3>`;
+    teamTwo.innerHTML = `<h3>${randomRight}</h3>`;
+    },1500);
 
 
 }   
 
 window.onload = function formatedTeams() {
 
-    const redTeam=JSON.parse(localStorage.getItem('redTeam'))
-    const greenTeam=JSON.parse(localStorage.getItem('greenTeam'))
-    const blackTeam=JSON.parse(localStorage.getItem('blackTeam'))
-    const whiteTeam=JSON.parse(localStorage.getItem('whiteTeam'))
+    const redTeam=JSON.parse(localStorage.getItem('redTeam'));
+    const greenTeam=JSON.parse(localStorage.getItem('greenTeam'));
+    const blackTeam=JSON.parse(localStorage.getItem('blackTeam'));
+    const whiteTeam=JSON.parse(localStorage.getItem('whiteTeam'));
 
-    console.log(redTeam)
-    console.log(greenTeam)
-    console.log(blackTeam)
-    console.log(whiteTeam)
+    console.log(redTeam);
+    console.log(greenTeam);
+    console.log(blackTeam);
+    console.log(whiteTeam);
 
     let redOne=document.querySelector('.red_one');
     let redTwo=document.querySelector('.red_two');
@@ -69,17 +66,16 @@ window.onload = function formatedTeams() {
     let whiteThree=document.querySelector('.white_three');
     let whiteFour=document.querySelector('.white_four');
 
+    redOne.innerText=redTeam[0].nome;
+    redTwo.innerText=redTeam[1].nome;
+    redThree.innerText=redTeam[2].nome;
+    redFour.innerText=redTeam[3].nome;
 
-    redOne.innerText=redTeam[0].nome
-    redTwo.innerText=redTeam[1].nome
-    redThree.innerText=redTeam[2].nome
-    redFour.innerText=redTeam[3].nome
 
-
-    greenOne.innerText=greenTeam[0].nome
-    greenTwo.innerText=greenTeam[1].nome
-    greenThree.innerText=greenTeam[2].nome
-    greenFour.innerText=greenTeam[3].nome
+    greenOne.innerText=greenTeam[0].nome;
+    greenTwo.innerText=greenTeam[1].nome;
+    greenThree.innerText=greenTeam[2].nome;
+    greenFour.innerText=greenTeam[3].nome;
 
     for (let i = 0; i < blackTeam.length; i++) {
         if (blackTeam[i] === null || blackTeam[i] === undefined) {
@@ -87,22 +83,20 @@ window.onload = function formatedTeams() {
         }
     }
 
-    
-    blackOne.innerText=blackTeam[0].nome
-    blackTwo.innerText=blackTeam[1].nome
-    blackThree.innerText=blackTeam[2].nome
-    blackFour.innerText=blackTeam[3].nome
+    blackOne.innerText=blackTeam[0].nome;
+    blackTwo.innerText=blackTeam[1].nome;
+    blackThree.innerText=blackTeam[2].nome;
+    blackFour.innerText=blackTeam[3].nome;
 
 
     for(let r=0; r< whiteTeam.length; r++) {
         if(whiteTeam[r] === null || whiteTeam[r] === undefined) {
-            whiteTeam.splice(r,1)
+            whiteTeam.splice(r,1);
         }
     }
 
-
-    whiteOne.innerText= whiteTeam[0].nome
-    whiteTwo.innerText= whiteTeam[1].nome
-    whiteThree.innerText= whiteTeam[2].nome
-    whiteFour.innerText= whiteTeam[3].nome
+    whiteOne.innerText= whiteTeam[0].nome;
+    whiteTwo.innerText= whiteTeam[1].nome;
+    whiteThree.innerText= whiteTeam[2].nome;
+    whiteFour.innerText= whiteTeam[3].nome;
 }
